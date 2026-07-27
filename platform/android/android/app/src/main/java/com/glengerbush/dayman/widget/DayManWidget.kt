e259021d680fb0bba9f849a9309acdb26c0bfa84
Glen Gerbush <glengerbush@gmail.com>
2026-07-27 01:26:12 -0400

    Fix Android widget activity action

--- platform/android/android/app/src/main/java/com/glengerbush/dayman/widget/DayManWidget.kt
+++ platform/android/android/app/src/main/java/com/glengerbush/dayman/widget/DayManWidget.kt
@@ -8,9 +8,9 @@ import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.action.clickable
+import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.Box
 import androidx.glance.layout.ContentScale
